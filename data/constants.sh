d7d4b3c83234d922fc51c4292781c594886a0e55
Ashish Baghudana <ashish.baghudana26@gmail.com>
2018-09-14 10:00:19 -0400

    Create fetch data script

---
+++ data/constants.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Networking Configuration
+export TEAM_NAME="cs4984cs5984f18_team10"
+export TEAM_ID="team10"
+export HADOOP_IP="hadoop.dlib.vt.edu"
+export PORT=2222
+
+# Data Paths
+export UNLABELED_DATA_PATH="/home/public/cs4984_cs5984_f18/unlabeled/data/ETD/golden_standards/$TEAM_ID"
